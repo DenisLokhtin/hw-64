5995c78b97048f3cc0075879e9dcf26df243cd81
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-08-27 14:51:40 +0600

    основная структура и стиили готовы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import {useState} from "react";
+import Home from "./Components/Home/Home";
+import Add from "./Components/Add/Add";
+import About from "./Components/About/About";
+import Contacts from "./Components/Contacts/Contacts";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h2 className="header">My Blog</h2>
+            <BrowserRouter>
+                <div className="navigation">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/Add">Add</NavLink>
+                    <NavLink to="/About">About</NavLink>
+                    <NavLink to="/Contacts">Contacts</NavLink>
+                </div>
+                <div className="container">
+                    <div className="container-inner">
+                        <Switch>
+                            <Route path="/" exact component={Home}/>
+                            <Route path="/Add" component={Add}/>
+                            <Route path="/About" component={About}/>
+                            <Route path="/Contacts" component={Contacts}/>
+                            <Route render={() => <h1>NOT FOUND</h1>}/>
+                        </Switch>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Add/Add.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Add.css'
+
+const Add = (props) => (
+    <div>
+        <h2>Add</h2>
+    </div>
+);
+
+export default Add;
